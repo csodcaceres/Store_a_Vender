6ac6d086ea879b880348ae2ddfbeedbac2150077
csodcaceres <cs.odcaceres@gmail.com>
2025-04-12 13:15:37 -0300

    Venta_x_categoria

---
+++ Venta_x_categoria.py
@@ -0,0 +1,31 @@
+# En este debes calcular la cantidad de productos vendidos por categoría en cada tienda. 
+# La idea es agrupar los datos por categoría y contar el número de ventas de cada tipo, 
+# mostrando las categorías más populares de cada tienda.
+
+import pandas as pd
+
+url = "https://raw.githubusercontent.com/alura-es-cursos/challenge1-data-science-latam/refs/heads/main/base-de-datos-challenge1-latam/tienda_1%20.csv"
+url2 = "https://raw.githubusercontent.com/alura-es-cursos/challenge1-data-science-latam/refs/heads/main/base-de-datos-challenge1-latam/tienda_2.csv"
+url3 = "https://raw.githubusercontent.com/alura-es-cursos/challenge1-data-science-latam/refs/heads/main/base-de-datos-challenge1-latam/tienda_3.csv"
+url4 = "https://raw.githubusercontent.com/alura-es-cursos/challenge1-data-science-latam/refs/heads/main/base-de-datos-challenge1-latam/tienda_4.csv"
+
+tienda = pd.read_csv(url)
+tienda2 = pd.read_csv(url2)
+tienda3 = pd.read_csv(url3)
+tienda4 = pd.read_csv(url4)
+
+
+
+# Analisis de ingresos
+ingreso_tienda1 = tienda['Precio'].sum()
+ingreso_tienda2 = tienda['Precio'].sum()
+ingreso_tienda3 = tienda['Precio'].sum()
+ingreso_tienda4 = tienda['Precio'].sum()
+
+# Ingresos totales
+ingresos_totales_df = pd.DataFrame({
+    'Tienda': ['Tienda 1', 'Tienda 2', 'Tienda 3', 'Tienda 4'],
+    'Ingreso Total': [ingreso_tienda1, ingreso_tienda2, ingreso_tienda3, ingreso_tienda4]
+})
+
+tienda.head()
